efe0265f676a92eac9e67b30c027aec2031969db
plan <frostyplanet@gmail.com>
2025-10-11 10:51:47 +0800

    Fix occams-rpc-tcp cargo.toml

--- transport_tcp/src/lib.rs
+++ transport_tcp/src/lib.rs
@@ -3,7 +3,7 @@
 
 //! # occams-rpc-tcp
 //!
-//! This crate provides a TCP transport implementation for [`occams-rpc`](https://docs.rs/occams-rpc).
+//! This crate provides a TCP transport implementation for [`occams-rpc-stream`](https://docs.rs/occams-rpc-stream).
 //! It is used for both client and server communication over TCP.
 
 #[macro_use]
